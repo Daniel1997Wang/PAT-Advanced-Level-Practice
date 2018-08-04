5812869e1b833a2e61dc45ba9b4fd16fbe0b8709
Daniel1997Wang <1822970791@qq.com>
2018-08-04 23:47:54 +0800

    修改第1011题目代码（删除冗余的代码及注释）

--- Practice/1011-1020/1011 World Cup Betting (20).py
+++ Practice/1011-1020/1011 World Cup Betting (20).py
@@ -20,18 +20,11 @@ def main():
     Second_Game = input().split(" ")
     Third_Game = input().split(" ")
 
-    #First_Game = ['1.1', '2.5', '1.7']
-    #Second_Game = ['1.2', '3.0', '1.6']
-    #Third_Game = ['4.1', '1.2', '1.1']
     #操作
 
     First_Game_max,First_Game_index = max_score(First_Game)
     Second_Game_max,Second_Game_index = max_score(Second_Game)
     Third_Game_max,Third_Game_index = max_score(Third_Game)
-
-    #print(First_Game_max,First_Game_index)
-    #print(Second_Game_max,Second_Game_index)
-    #print(Third_Game_max,Third_Game_index)
 
     max_value = round(First_Game_max * Second_Game_max * Third_Game_max,3)
     maximum_profit = (max_value * 0.65 - 1) * 2
@@ -44,7 +37,6 @@ def main():
     print(Result[Third_Game_index],end="")
     print(" ",end="")
     print(round(maximum_profit,2),end="")
-
 
 
 if __name__ == "__main__":
