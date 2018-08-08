6c43e20d6c2e656e92459168582a193e39a7de09
Daniel1997Wang <1822970791@qq.com>
2018-08-08 17:23:17 +0800

    提交1023题代码

---
+++ Practice/1021-1030/1023 Have Fun with Numbers (20).py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main():
+    #输入
+    #N = input()
+    N = "1234567899"
+    #操作、输出
+    Change_N = str(2*int(N))
+    print(N,Change_N)
+
+
+
+
+if __name__ == "__main__":
+    main()
