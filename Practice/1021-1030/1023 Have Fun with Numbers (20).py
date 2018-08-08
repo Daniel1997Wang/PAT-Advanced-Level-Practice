40840532892a258406552f94bc0f736e20d38d57
Daniel1997Wang <1822970791@qq.com>
2018-08-08 17:43:38 +0800

    修改1023题代码

--- Practice/1021-1030/1023 Have Fun with Numbers (20).py
+++ Practice/1021-1030/1023 Have Fun with Numbers (20).py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+N_count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+Change_N_count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
 def main():
     #输入
-    #N = input()
-    N = "1234567899"
-    #操作、输出
-    Change_N = str(2*int(N))
-    print(N,Change_N)
+    N = input()
+    #操作
 
+    for i in range(len(N)):
+        N_count[int(N[i])] += 1
+
+    Change_N = str(2*int(N))
+    for i in range(len(N)):
+        Change_N_count[int(Change_N[i])] += 1
+
+    Flag = 1
+    for i in range(10):
+        if(N_count[i] != Change_N_count[i]):
+            Flag = 0
+
+    #输出
+    if(Flag):
+        print("Yes")
+    else:
+        print("No")
+
+    print(Change_N, end="")
 
 
 
